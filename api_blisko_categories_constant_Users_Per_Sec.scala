2a2bf4a4686b16e381836989639331b7a58f12c4
PiotrGorecki79 <piotr.gorecki@pracuj.pl>
2015-09-23 14:58:33 +0200

    Zmiana 2
    Zmiana nazwy

--- api_blisko_categories_constant_Users_Per_Sec.scala
+++ api_blisko_categories_constant_Users_Per_Sec.scala
@@ -4,7 +4,7 @@ import io.gatling.core.Predef._ // 2
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class api_blisko_categories_constant_Users_Per_Sec extends Simulation { // 3
+class api_blisko_categories_constant_Users_Per_Sec_2 extends Simulation { // 3
 
   val httpConf = http // 4
     .baseURL("https://api-blisko.pracuj.pl/categories") // 5
@@ -14,7 +14,7 @@ class api_blisko_categories_constant_Users_Per_Sec extends Simulation { // 3
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("api_blisko_categories_constant_Users_Per_Sec") // 7
+  val scn = scenario("api_blisko_categories_constant_Users_Per_Sec_2") // 7
     .exec(http("request_1")  // 8
     .get("/")) // 9
     .pause(5) // 10
